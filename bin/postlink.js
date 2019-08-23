56ebe8edc17fa3a4130957731969e24a3519d26f
phillipc-myfiziq <phillip@myfiziq.com>
2019-08-23 14:30:43 +0800

    Update script to fix out-of-order issue

--- bin/postlink.js
+++ bin/postlink.js
@@ -29,6 +29,38 @@ if (package.needsSwiftFix) needsSwiftFix = true;
 const doFix = isSwift && needsSwiftFix;
 const mydir = process.cwd();
 process.chdir("./ios");
+
+function updateXcodeProject(callback) {
+  const g = path.join(process.cwd(), "**", "project.pbxproj");
+  glob.sync(g).forEach(path => {
+    console.log("Found pbxproj file at ", path);
+    const project = pbxproj.project(path);
+    project.parse(function (err) {
+      console.log("Found Target ", project.getFirstTarget().firstTarget.name);
+      var opt = { target : project.getFirstTarget().uuid };
+      // project.pbxCreateGroup("RNMyFiziqSdk", "RNMyFiziqSdk");
+      // var group = project.findPBXGroupKey({name:'RNMyFiziqSdk'});
+      project.addHeaderFile('RNMyFiziqSdk.h', opt);
+      project.addSourceFile('RNMyFiziqSdk.m', opt);
+      project.addHeaderFile('RNMyFiziqWrapAvatar.h', opt);
+      project.addSourceFile('RNMyFiziqWrapAvatar.m', opt);
+      project.addHeaderFile('RNMyFiziqWrapCommon.h', opt);
+      project.addSourceFile('RNMyFiziqWrapCommon.m', opt);
+      project.addHeaderFile('RNMyFiziqWrapCore.h', opt);
+      project.addSourceFile('RNMyFiziqWrapCore.m', opt);
+      project.addHeaderFile('RNMyFiziqWrapUser.h', opt);
+      project.addSourceFile('RNMyFiziqWrapUser.m', opt);
+      project.addHeaderFile('RNMyFiziqWrapCognitoAuth.h', opt);
+      project.addSourceFile('RNMyFiziqWrapCognitoAuth.m', opt);
+      fs.writeFileSync(path, project.writeSync());
+      console.log('Add MyFiziq source files...done');
+      if (callback) {
+        callback();
+      }
+    });
+  });
+}
+
 // Copy in the MYQ binding files
 fs.copyFile(nodepath + "/react-native-my-fiziq-sdk/ios/RNMyFiziqSdk.h", "RNMyFiziqSdk.h", (err) => {if (err) throw err;});
 fs.copyFile(nodepath + "/react-native-my-fiziq-sdk/ios/RNMyFiziqSdk.m", "RNMyFiziqSdk.m", (err) => {if (err) throw err;});
@@ -43,37 +75,15 @@ fs.copyFile(nodepath + "/react-native-my-fiziq-sdk/ios/RNMyFiziqWrapUser.m", "RN
 fs.copyFile(nodepath + "/react-native-my-fiziq-sdk/ios/RNMyFiziqWrapCognitoAuth.h", "RNMyFiziqWrapCognitoAuth.h", (err) => {if (err) throw err;});
 fs.copyFile(nodepath + "/react-native-my-fiziq-sdk/ios/RNMyFiziqWrapCognitoAuth.m", "RNMyFiziqWrapCognitoAuth.m", (err) => {if (err) throw err;});
 // Update XCode project with new files
-const g = path.join(process.cwd(), "**", "project.pbxproj");
-glob.sync(g).forEach(path => {
-  console.log("Found pbxproj file at ", path);
-  const project = pbxproj.project(path);
-  project.parse(function (err) {
-    console.log("Found Target ", project.getFirstTarget().firstTarget.name);
-    var opt = { target : project.getFirstTarget().uuid };
-    // project.pbxCreateGroup("RNMyFiziqSdk", "RNMyFiziqSdk");
-    // var group = project.findPBXGroupKey({name:'RNMyFiziqSdk'});
-    project.addHeaderFile('RNMyFiziqSdk.h', opt);
-    project.addSourceFile('RNMyFiziqSdk.m', opt);
-    project.addHeaderFile('RNMyFiziqWrapAvatar.h', opt);
-    project.addSourceFile('RNMyFiziqWrapAvatar.m', opt);
-    project.addHeaderFile('RNMyFiziqWrapCommon.h', opt);
-    project.addSourceFile('RNMyFiziqWrapCommon.m', opt);
-    project.addHeaderFile('RNMyFiziqWrapCore.h', opt);
-    project.addSourceFile('RNMyFiziqWrapCore.m', opt);
-    project.addHeaderFile('RNMyFiziqWrapUser.h', opt);
-    project.addSourceFile('RNMyFiziqWrapUser.m', opt);
-    project.addHeaderFile('RNMyFiziqWrapCognitoAuth.h', opt);
-    project.addSourceFile('RNMyFiziqWrapCognitoAuth.m', opt);
-    fs.writeFileSync(path, project.writeSync());
-    console.log('Add MyFiziq source files...done');
-  });
+updateXcodeProject(() => {
+  // Pod install
+  spawnSync("pod", ["install"], opts);
+  if (doFix) {
+    const fixPods = require("../lib/fixPods");
+    process.chdir(mydir);
+    fixPods();
+  }
+  // Remove duplicate React package.json
+  spawnSync("rm", ["-f", "Pods/React/package.json"]);
 });
-// Pod install
-spawnSync("pod", ["install"], opts);
-if (doFix) {
-  const fixPods = require("../lib/fixPods");
-  process.chdir(mydir);
-  fixPods();
-}
-// Remove duplicate React package.json
-spawnSync("rm", ["-f", "Pods/React/package.json"]);
+
